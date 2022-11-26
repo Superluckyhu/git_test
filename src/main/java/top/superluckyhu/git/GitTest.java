5e5b520214e24af3996b6d070c195eccf1f94bad
Superluckyhu <Superluckyhu@163.com>
2022-11-27 01:01:47 +0800

    IDEA Git Commit Test V1.0

---
+++ src/main/java/top/superluckyhu/git/GitTest.java
@@ -0,0 +1,14 @@
+package top.superluckyhu.git;
+
+/**
+ * @ClassName GitTest
+ * @Description TODO
+ * @Author Superluckyhu
+ * @Date 2022/11/27 0:51
+ * @Version 1.0
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!!!");
+    }
+}
